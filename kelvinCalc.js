8c3a4d906eb591e73aba67cac2b7108e1d7ce2c8
loganed09 <64222358+loganed09@users.noreply.github.com>
2021-09-27 11:08:13 -0400

    Create kelvinCalc.js

---
+++ kelvinCalc.js
@@ -0,0 +1,14 @@
+// kelvin
+const kelvin = 0;
+
+// celcius
+let celcius = kelvin - 273;
+// fahrenheit
+let fahrenheit = Math.floor(celcius * (9/5) + 32);
+
+let Newton = Math.floor(celcius * (33/100));
+
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit. And ${Newton} degrees Newton.`);
+
+
